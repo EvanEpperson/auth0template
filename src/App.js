73b8e859b138b0070a5ea2b9d6bf7fbd671ac06b
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-02 21:44:07 -0600

    made login button , making logout next

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import React from 'react';
 import './App.css';
+import LoginButton from './components/LoginButton'
 
 function App() {
   return (
-    <div className="App">
-      <h1>testing</h1>
-    </div>
+    <LoginButton />
   );
 }
 
