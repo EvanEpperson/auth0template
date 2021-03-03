2dff912a9288e888bf0be5479b98827c5721acad
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-02 21:49:41 -0600

    made logout button

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -4,9 +4,9 @@ import {useAuth0} from '@auth0/auth0-react'
 const LogoutButton = () => {
     const {logout } = useAuth0()
     return (
-        <div>
-            
-        </div>
+            <button onClick={() => logout()}>
+                Logout
+            </button>
     )
 }
 
